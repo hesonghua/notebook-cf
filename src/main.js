513ce7a9a81b96961dc94a52aeacc7226ab9c830
victor <victor@qmod.cc>
2025-10-23 02:51:47 +0800

    set marked sanitize option to false global

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { useConfigStore } from './stores/configStore'
+import { marked } from 'marked'
 
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 import mitt from 'mitt';
+
+// 在应用启动时，全局配置 marked
+marked.setOptions({
+  sanitize: false,
+});
 
 const app = createApp(App)
 const emitter = mitt();
